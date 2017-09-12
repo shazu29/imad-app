d26d86792a5bf457077ddcb6cb13f1c385be88e4
shazu29 <shasal.1703@gmail.com>
2017-09-12 12:39:12 +0200

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,11 +18,11 @@ app.get('/article1', function (req, res) {
 });
 
 app.get('/article2', function (req, res) {
-    res.send("article two reqested will be served here");
+   res.sendFile(path.join(__dirname, 'ui', 'article2.html'));
 });
 
 app.get('/article3', function (req, res) {
-    res.send("article three reqested will be served here");
+   res.sendFile(path.join(__dirname, 'ui', 'article3.html'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
